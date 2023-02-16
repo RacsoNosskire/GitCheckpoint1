17647fc59923feafb7474ad44c761238d7852223
RacsoNosskire <124093099+RacsoNosskire@users.noreply.github.com>
2023-02-16 14:24:01 +0100

    Added CheckTest Class.

--- GitCheckpoint1/Program.cs
+++ GitCheckpoint1/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            string test = "Task 2 checked";
             Console.WriteLine("This is the Git checkpoint task 1.1");
         }
     }
